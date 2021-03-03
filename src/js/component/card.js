986f88c0f04d39787a224402d21f9faa3b95d2c4
ricubi199 <cubilloricardo@outlook.es>
2021-03-03 21:02:22 +0000

    Create card component

---
+++ src/js/component/card.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+export function Card() {
+	return (
+		<div className="card-group">
+			<div className="card">
+				<img
+					src="http://placehold.it/500x325"
+					className="card-img-top"
+					alt="..."></img>
+				<h5 className="card-title">Card tittle</h5>
+				<p className="card-text">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+					pharetra massa risus, ac dignissim nisl ullamcorper quis.
+				</p>
+				<a href="#" className="btn btn-primary">
+					Find out More!
+				</a>
+			</div>
+			<div className="card">
+				<img
+					src="http://placehold.it/500x325"
+					className="card-img-top"></img>
+				<h5 className="card-title">Card tittle</h5>
+				<p className="card-text">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+					pharetra massa risus, ac dignissim nisl ullamcorper quis.
+				</p>
+				<a href="#" className="btn btn-primary">
+					Find out More!
+				</a>
+			</div>
+			<div className="card">
+				<img
+					src="http://placehold.it/500x325"
+					className="card-img-top"></img>
+				<h5 className="card-title">Card tittle</h5>
+				<p className="card-text">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+					pharetra massa risus, ac dignissim nisl ullamcorper quis.
+				</p>
+				<a href="#" className="btn btn-primary">
+					Find out More!
+				</a>
+			</div>
+			<div className="card">
+				<img
+					src="http://placehold.it/500x325"
+					className="card-img-top"></img>
+				<h5 className="card-title">Card tittle</h5>
+				<p className="card-text">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+					pharetra massa risus, ac dignissim nisl ullamcorper quis.
+				</p>
+				<a href="#" className="btn btn-primary">
+					Find out More!
+				</a>
+			</div>
+		</div>
+	);
+}
